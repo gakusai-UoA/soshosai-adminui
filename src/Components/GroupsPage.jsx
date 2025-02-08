1b9e3b0a20489fad2e659a2ca958accc823ada87
shakenokirimi12 <ken20051205@gmail.com>
2025-02-08 16:27:08 +0900

    グループページでURLパラメータに基づく検索用語の設定を修正

--- src/Components/GroupsPage.jsx
+++ src/Components/GroupsPage.jsx
@@ -24,7 +24,7 @@ const GroupsPage = () => {
     if (id) {
       setSearchTerm(id);
     }
-  }, [location.search, tickets]);
+  }, [location.search, groups]);
 
   const navigate = useNavigate();
 
